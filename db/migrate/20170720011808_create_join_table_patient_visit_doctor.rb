b05b333c9420bc1c74bc2eb0db9f3a45f846999b
Emmanuel Addo-Yirenkyi <eyirenkyi@claimsync.com>
2017-07-25 04:56:55 +0100

    Changes were made

--- db/migrate/20170720011808_create_join_table_patient_visit_doctor.rb
+++ db/migrate/20170720011808_create_join_table_patient_visit_doctor.rb
@@ -1,9 +1,8 @@
 class CreateJoinTablePatientVisitDoctor < ActiveRecord::Migration
   def change
-    
     create_join_table :Patient_Visits, :Doctors do |t|
-     t.index [:patient_visit_id, :doctor_id]
-     t.index [:doctor_id, :patient_visit_id]
+      t.index [:patient_visit_id, :doctor_id]
+      t.index [:doctor_id, :patient_visit_id]
     end
   end
 end
