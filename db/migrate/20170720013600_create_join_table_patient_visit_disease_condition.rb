8cb2ad4f8049ce218431efdd0ade304f250de5b5
Emmanuel Addo-Yirenkyi <eyirenkyi@claimsync.com>
2017-07-25 05:07:20 +0100

    Update of Migrations

--- db/migrate/20170720013600_create_join_table_patient_visit_disease_condition.rb
+++ db/migrate/20170720013600_create_join_table_patient_visit_disease_condition.rb
@@ -2,8 +2,8 @@ class CreateJoinTablePatientVisitDiseaseCondition < ActiveRecord::Migration
   def change
 
     create_join_table :Patient_Visits, :DiseaseConditions do |t|
-       t.index [:patient_visit_id, :disease_condition_id]
-       t.index [:disease_condition_id, :patient_visit_id]
+       #t.index [:patient_visit_id, :disease_condition_id]
+       #t.index [:disease_condition_id, :patient_visit_id]
     end
   end
 end
